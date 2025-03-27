5e3093f6c7661f9ec9f81f41de16e708c19145f2
GisatAZK2 <gisatazk2@gmail.com>
2025-03-27 10:37:52 +0700

    Login session

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -45,5 +45,17 @@ class AuthController extends Controller
     public function login()
     {
         //for login
+        $credentials = request(['email', 'password']);
+
+        if (!$token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+    
+        return response()->json([
+            'message' => 'Login success',
+            'token' => $token
+        ]);
+
+
     }
 }
