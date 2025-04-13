4a51b46661a8e51fd33cef9c62b35c30f0cb573a
Ridy <149645280+RidyCh@users.noreply.github.com>
2025-04-13 17:10:13 +0700

    fix(docs): predict, student

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -1,11 +1,12 @@
 <?php
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Controllers\Controller;
-use App\Models\Student;
 use App\Models\User;
-use Illuminate\Http\Request;
+use App\Models\Student;
 use Illuminate\Support\Arr;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
