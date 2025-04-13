56823872c0cc7a1996500b537e9391c2c287ac91
Ridy <149645280+RidyCh@users.noreply.github.com>
2025-04-13 19:52:59 +0700

    fix(routes): update method student routes

--- routes/api/v1/students.php
+++ routes/api/v1/students.php
@@ -1,8 +1,9 @@
+
 <?php
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\StudentController;
 
 Route::post('/', [StudentController::class, 'store']);
-Route::post('/public-student', [StudentController::class, 'publicStudents']);
+Route::get('/public-student', [StudentController::class, 'publicStudents']);
 Route::get('/student-detail', [StudentController::class, 'showStudentForGuest']);
