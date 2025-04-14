c0fd837a6f06d86df9907c9baa646d8fc048fad4
Ridy <149645280+RidyCh@users.noreply.github.com>
2025-04-14 15:04:21 +0700

    fix style swagger ui

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,8 +14,8 @@ sed -i "s/<VirtualHost \*:80>/<VirtualHost *:${PORT}>/g" /etc/apache2/sites-avai
 # Laravel setup commands
 php artisan config:clear
 php artisan config:cache
-php artisan migrate:fresh --force
-php artisan db:seed --class=DatabaseSeeder --force
+# php artisan migrate --force
+# php artisan db:seed --class=DatabaseSeeder --force
 php artisan filament:optimize
 
 # Publish Filament and Swagger assets
